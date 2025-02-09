1314ba124d1d79cf1462e99ba5cc1ca523622789
Maksim Fisenko <fisenkomaksim.id@gmail.com>
2025-02-09 16:40:58 +0300

    :star: feat: add readIDParam helper

--- cmd/api/courses.go
+++ cmd/api/courses.go
@@ -8,15 +8,21 @@ import (
 )
 
 func (app *application) showCourseHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
 	course := data.Course{
-		ID:        1,
+		ID:        id,
 		Title:     "Course Name 1",
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 		Version:   1,
 	}
 
-	err := app.writeJSON(w, http.StatusOK, course, nil)
+	err = app.writeJSON(w, http.StatusOK, course, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
