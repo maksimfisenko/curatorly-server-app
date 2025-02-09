787edf381db0eef60846956b9d26020739a69a29
Maksim Fisenko <fisenkomaksim.id@gmail.com>
2025-02-09 17:19:12 +0300

    :star: feat: add bad request helper

--- cmd/api/courses.go
+++ cmd/api/courses.go
@@ -36,7 +36,7 @@ func (app *application) createCourseHandler(w http.ResponseWriter, r *http.Reque
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
