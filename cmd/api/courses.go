6605c5fd4f56d0b68c7ec7e00e96d2cc1801146d
Maksim Fisenko <fisenkomaksim.id@gmail.com>
2025-02-09 16:48:11 +0300

    :star: feat: add response enveloping

--- cmd/api/courses.go
+++ cmd/api/courses.go
@@ -22,7 +22,7 @@ func (app *application) showCourseHandler(w http.ResponseWriter, r *http.Request
 		Version:   1,
 	}
 
-	err = app.writeJSON(w, http.StatusOK, course, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"course": course}, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
