303a5461473ea829037cc1e4e2e0c60d4a17272a
Maksim Fisenko <fisenkomaksim.id@gmail.com>
2025-03-08 17:58:53 +0300

    :star: feat: add writeJSON helper

---
+++ cmd/api/helpers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"maps"
+	"net/http"
+)
+
+type envelope map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	maps.Copy(w.Header(), headers)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
