48cf78c7b896196eddda77a3c50ebed569ce1b38
Maksim Fisenko <fisenkomaksim.id@gmail.com>
2025-03-25 22:09:37 +0300

    :up: update: update data types in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -72,15 +72,15 @@ func main() {
 
 	expvar.NewString("version").Set(version)
 
-	expvar.Publish("goroutines", expvar.Func(func() interface{} {
+	expvar.Publish("goroutines", expvar.Func(func() any {
 		return runtime.NumGoroutine()
 	}))
 
-	expvar.Publish("database", expvar.Func(func() interface{} {
+	expvar.Publish("database", expvar.Func(func() any {
 		return db.Stats()
 	}))
 
-	expvar.Publish("timestamp", expvar.Func(func() interface{} {
+	expvar.Publish("timestamp", expvar.Func(func() any {
 		return time.Now().Unix()
 	}))
 
